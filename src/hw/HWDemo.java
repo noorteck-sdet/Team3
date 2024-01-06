360c6f7184c9558ae8fa2763b9151d982c17f8c3
ehsan <ehsan@ehsans-air.hsd1.tn.comcast.net>
2024-01-06 15:38:02 -0500

    organized

--- src/hw/HWDemo.java
+++ src/hw/HWDemo.java
@@ -10,21 +10,40 @@ public class HWDemo {
 	public static void main(String[] args) {
 
 		
-		System.out.println("Cats are the best");
+		System.out.println("Dogs are the best");
 		
 		// java is fun
 		
 		// i like java
 		
+		// this is going well--Brian
+		
+		//Ahmed Aly added this ArrayList
+				ArrayList <String> marketTeam3 = new ArrayList <> (); 
 
+			marketTeam3.add("Ahmed");
+			marketTeam3.add("Alishan");
+			marketTeam3.add("Brian");
+			marketTeam3.add("Cindy");
+			marketTeam3.add("Kamy");
+			marketTeam3.add("Ellen");
+			marketTeam3.add("Jackie");
+			marketTeam3.add("Sabina");
+			marketTeam3.add("Ehsen");
+			marketTeam3.add("Akbr");
+			marketTeam3.add("Kayla");
+			marketTeam3.add("Soli");
+			marketTeam3.add("Faraz");
+	System.out.println(marketTeam3);
 
-		System.out.println("Cats are the best");// Ellen
+				
+		
+       //I can't wait to hit the market Ahmed "coming from gitHub"
 		
 		HWDemo obj = new HWDemo();
 		
 		System.out.println(obj.isEven(8));
 		System.out.println(obj.isEven(3));
-
 
 	}
 	public void subtract(int a, int b) {
@@ -32,19 +51,24 @@ public class HWDemo {
 	}
 
 	public void dog(String bark) {
+
 		System.out.println("bark");
-
-	
-
-
+	// alishan added this
 	}
 	//Solia
 	public static void add(int a, int b){
 		System.out.println(a+b); 
 	} 
 	
+	public static void num(String name) {
+
+		// this is from git to java, alishan
+		
+	}
+	
 	public boolean isEven(int num) {
 		if(num%2==0) return true;
 		return false;
 	}
+	//all the line spacing in the code drives me nuts! -Ellen
 }
