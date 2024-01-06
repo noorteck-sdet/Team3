d504c2768ac45883f46705912efc1d2d487019a3
ehsan <ehsan@ehsans-air.hsd1.tn.comcast.net>
2024-01-05 22:37:48 -0500

    removed Cindy's method and added count100()

--- src/hw/HWDemo.java
+++ src/hw/HWDemo.java
@@ -2,28 +2,29 @@ package hw;
 
 public class HWDemo {
 
-	//Instruction: Each team member pushes at least once by adding 1 method
-	//Practice Cloning, Pulling, Pushing, Stashing
-	
+	// Instruction: Each team member pushes at least once by adding 1 method
+	// Practice Cloning, Pulling, Pushing, Stashing
+
 	public static void main(String[] args) {
-		
-		System.out.println("Dogs are the best");//Ellen
-		
+
+		System.out.println("Dogs are the best");// Ellen
+
 		count100();
-	
-	
+
 	}
-	
-	
+
 	public static void count100() {
 		int count = 1;
-		for(int i = 0; i < 100; i++) {
+		for (int i = 0; i < 100; i++) {
 			System.out.println(count);
 			count++;
 		}
 	}
-	
+
 	public void dog(String bark) {
 		System.out.println("bark");
+
+	
+
 	}
 }
