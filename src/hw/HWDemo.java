4f8c179c4bf572ebb05f2bbe91751e47bbe4bb64
ehsan <ehsan@ehsans-air.hsd1.tn.comcast.net>
2024-01-07 23:16:30 -0500

    final changes

--- src/hw/HWDemo.java
+++ src/hw/HWDemo.java
@@ -9,127 +9,124 @@ public class HWDemo {
 
 	public static void main(String[] args) {
 
-		
+		System.out.println("Dogs are the best");// Ellen
 
-		System.out.println("Dogs are the best");
-
-		System.out.println("Dogs are the best");//Ellen
-
-		
 		// java is fun
-		
+
 		// i like java
-		
+
 		// this is going well--Brian
-		
-		//Ahmed Aly added this ArrayList
-				ArrayList <String> marketTeam3 = new ArrayList <> (); 
 
-			marketTeam3.add("Ahmed");
-			marketTeam3.add("Alishan");
-			marketTeam3.add("Brian");
-			marketTeam3.add("Cindy");
-			marketTeam3.add("Kamy");
-			marketTeam3.add("Ellen");
-			marketTeam3.add("Jackie");
-			marketTeam3.add("Sabina");
-			marketTeam3.add("Ehsen");
-			marketTeam3.add("Akbr");
-			marketTeam3.add("Kayla");
-			marketTeam3.add("Soli");
-			marketTeam3.add("Faraz");
-	System.out.println(marketTeam3);
+		// Ahmed Aly added this ArrayList
+		ArrayList<String> marketTeam3 = new ArrayList<>();
 
+		marketTeam3.add("Ahmed");
+		marketTeam3.add("Alishan");
+		marketTeam3.add("Brian");
+		marketTeam3.add("Cindy");
+		marketTeam3.add("Kamy");
+		marketTeam3.add("Ellen");
+		marketTeam3.add("Jackie");
+		marketTeam3.add("Sabina");
+		marketTeam3.add("Ehsen");
+		marketTeam3.add("Akbr");
+		marketTeam3.add("Kayla");
+		marketTeam3.add("Soli");
+		marketTeam3.add("Faraz");
+		System.out.println(marketTeam3);
 
-				
-		
+		// I can't wait to hit the market Ahmed "coming from gitHub"
 
-		System.out.println("Dogs are the best");//Ellen
-		
-		
-       //I can't wait to hit the market Ahmed "coming from gitHub"
-		
 		HWDemo obj = new HWDemo();
-		
+
 		System.out.println(obj.isEven(8));
 		System.out.println(obj.isEven(3));
 		System.out.println(obj.isEven(9));
+		
+		hw();
 
 	}
+
 	public void subtract(int a, int b) {
 		System.out.println(a - b);
 	}
 
+	// trying to fill up space, but stash it - Ellen
 
-
-	//trying to fill up space, but stash it - Ellen
-	
-	//just trying to fill in the space!-Ellen
-
+	// just trying to fill in the space!-Ellen
 
 	public void dog(String bark) {
 
 		System.out.println("bark");
-	// alishan added this
+		// Alishan added this
 	}
-	//Solia
-	public static void add(int a, int b){
-		System.out.println(a+b); 
-	} 
-	
+
+	// Solia
+	public static void add(int a, int b) {
+		System.out.println(a + b);
+	}
+
 	public static void num(String name) {
 
-		// this is from git to java, alishan
-		
+		// this is from Git to java, Alishan
+
 	}
-	
+
 	public boolean isEven(int num) {
-		if(num%2==0) return true;
+		if (num % 2 == 0)
+			return true;
 		return false;
 	}
-	//all the line spacing in the code drives me nuts! -Ellen
-	
+	// all the line spacing in the code drives me nuts! -Ellen
 
-	//pulling from GitHub
+	// pulling from GitHub
 
 	public static void multiply(int c, int d) {
-		System.out.println(c * d);
+		System.out.println(c / d);
 
 		// hello from Git - Sabina
 	}
 
 	public static void GitHub(int a, String git) {
-		System.out.println(a + "git");//Brian did this
+		System.out.println(a + "git");// Brian did this
 	}
-	public static void divide(int e, int f){
-		System.out.println(e/f);//Ellen
-		
-		//stashing again
-		
-		//pulling from Git
 
-		System.out.println("Sunday Funday");//Ellen
+	public static void divide(int e, int f) {
+		System.out.println(e / f);// Ellen
+
+		// stashing again
+
+		// pulling from Git
+
+		System.out.println("Sunday Funday");// Ellen
 	}
 
 	// Git to Java--Brian
 	String str = "We're going to crush it!";
+
 	public static void jobMarket(String str) {
-		
+
 		System.out.println(str);
 	}
-	//Hope Everything Is Good SOLIANA 
-	
-	
-	//Kayla is pushing to team 3
-//pulling from Gethub
-	
-		public void comment() {
-			System.out.println("Demo Comment");
-	// Print Demo Comment
+	// Hope Everything Is Good SOLIANA
+
+	// Kayla is pushing to team 3
+	//pulling from GitHub
+
+	public void comment() {
+		System.out.println("Demo Comment");
+		// Print Demo Comment
 
 	}
 
 	public void myMethod() {
 		System.out.println("Demo Method");
+		// print Demo Method
 	}
+	
+	public static final String hw() {
+		return  "Homework is done...";
+	}
+	
+	
 }
